fb05e63b3a895ec72f8d2fd5e1d4842648102184
Jón Rúnar Helgason <dev@jonrh.is>
2017-08-04 13:58:20 +0100

    Updated About Me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,9 +19,9 @@ export default class About extends React.Component {
 
         <ul>
           <li>Independent dashboard consultant & frontend software engineer</li>
-          <li>Available for contract work in September 2017.</li>
+          <li>Available for contract work in mid September 2017.</li>
           <li>Currently working on two iOS and Android mobile dashboards written in React Native.</li>
-          <li>Very interested in functional programming: Elm, PureScript, ReasonML, Haskell, etc.</li>
+          <li>Very interested in functional programming: ReasonML, Elm, PureScript, Haskell, etc.</li>
           <li>Icelander working remotely from Galway, Ireland.</li>
         </ul>
 
@@ -29,21 +29,21 @@ export default class About extends React.Component {
           <li>Created the <Link to={"/retrospective-on-react"}>Biomass Daily web app</Link> for <a href="http://vaki.is/">Vaki</a>, interactive dashboard.</li>
           <li>Frontend for 3 years in JavaScript, React & React Native.</li>
           <li>Backend for 2 years (Python/Java/SQL) but found passion in the rapidly changing frontend.</li>
-          <li>MSc Computer Science from <a href="https://www.ucd.ie/">University College Dublin</a>.</li>
-          <li>BSc Computer Science from <a href="https://en.ru.is/">Reykjavík University</a>.</li>
+          <li>MSc in Computer Science from <a href="https://www.ucd.ie/">University College Dublin</a>.</li>
+          <li>BSc in Computer Science from <a href="https://en.ru.is/">Reykjavík University</a>.</li>
         </ul>
 
         <p>
           <strong>Services</strong>:
-          I help companies make informed business decisions based on their data. To do that I’ve
-          specialised in creating tailor made dashboard websites and mobile apps. We will be a good
-          fit if you:
+          I help companies create bespoke dashboards their customers want to use. Essentially I
+          take care of the frontend while your company can focus on a good backend. We will be a
+          good fit if you:
         </p>
 
         <ul>
           <li>Require a tailor made dashboard to make good use of your company’s data.</li>
           <li>Have in-house backend expertise to serve your data through an API.</li>
-          <li>Have a good idea what your KPIs are and what to filter by.</li>
+          <li>Have a good idea what your KPIs are.</li>
           <li>Want to offload entirely the frontend work & responsibilities.</li>
         </ul>
 
