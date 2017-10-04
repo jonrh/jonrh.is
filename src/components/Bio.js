43ee5c1775e0dc873158723cbcaa89af0c5c9183
Jón Rúnar Helgason <dev@jonrh.is>
2017-10-04 15:13:17 +0100

    Updated circular profile picture

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from "react";
 import "typeface-montserrat";
 import "typeface-merriweather";
 
-import profilePic from "./profile-pic.png";
+import profilePic from "./profile-pic-2017.png";
 import { rhythm } from "../utils/typography";
 
 class Bio extends React.Component {
