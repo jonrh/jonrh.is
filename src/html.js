41dbccb350c8b40515d6d8f5983173ce9442150e
Jón Rúnar Helgason <dev@jonrh.is>
2018-01-04 20:50:24 +0000

    Include Rollbar if the we are running in the production environment

--- src/html.js
+++ src/html.js
@@ -2,6 +2,7 @@ import React from "react";
 import { TypographyStyle } from "react-typography";
 import Helmet from "react-helmet";
 
+import Rollbar from "./components/Rollbar";
 import typography from "./utils/typography";
 import favicon from "./pages/favicon.ico";
 
@@ -18,9 +19,14 @@ export default class Html extends React.Component {
   render() {
     const head = Helmet.rewind();
     let css;
+
     if (process.env.NODE_ENV === `production`) {
       css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }} />;
     }
+
+    // When we include Rollbar error reporting, it goes on top of the error stack. When
+    // developing I want to be able to see in the browser console where the error came from.
+    const includeRollbarIfProduction = process.env.NODE_ENV === `production` ? Rollbar : null;
 
     return (
       <html lang="en">
@@ -28,6 +34,7 @@ export default class Html extends React.Component {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        { includeRollbarIfProduction }
         <link rel="shortcut icon" href={favicon} />
         {this.props.headComponents}
         <TypographyStyle typography={typography} />
