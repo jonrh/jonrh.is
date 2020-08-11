14b507e646bc3b699a6614655109d427a7bf0b5b
Jón Rúnar Helgason <dev@jonrh.is>
2020-08-11 09:59:48 +0100

    Converted 6October 2007 mix from Markdown to JSX

--- src/pages/6october-2007-mix/index.js
+++ src/pages/6october-2007-mix/index.js
@@ -1,26 +1,37 @@
----
-title: 6October 2007 mix
-date: "2007-10-06T00:00:00Z"
-path: "/6october-2007-mix/"
----
+import React from "react";
+import Layout from "./../../layouts";
 
-Download link: 6October 2007 mix
+const Post = () => {
+ return (
+   <Layout>
+    <h1>6October 2007 mix</h1>
+    <p>October 06, 2007</p>
+    <p>Download link: 6October 2007 mix</p>
 
-## Tracklisting
+    <h2>Tracklisting</h2>
 
-1. Nathan Fake – “The Sky Was Pink (Holden remix)” [Border Community] – 0:00
-2. Glenn Morrison – “Hydrology” [mau5trap] – 2:15
-3. Glenn Morrison – “Contact” [mau5trap] – 5:15
-4. Adam K, Soha – “Twilight” [Hotbox Digital] – 8:00
-5. Deadmau5 – “Not Exactly” [mau5trap] – 10:20
-6. Innate – “Imod (Jerome Isma-Ae remix)” [Baroque Limited Records] – 12:40
-7. Deadmau5 – “Faxing Berlin (Chris Lake edit)” [mau5trap] – 16:40
-8. Deadmau5 – “Jaded” [mau5trap] – 19:40
-9. AxEr – “321” [Pryda Friends] – 23:00
-10. Pryda – “Muranyi” [Pryda Friends] – 26:35
+    <ol>
+      <li>Nathan Fake – “The Sky Was Pink (Holden remix)” [Border Community] – 0:00</li>
+      <li>Glenn Morrison – “Hydrology” [mau5trap] – 2:15</li>
+      <li>Glenn Morrison – “Contact” [mau5trap] – 5:15</li>
+      <li>Adam K, Soha – “Twilight” [Hotbox Digital] – 8:00</li>
+      <li>Deadmau5 – “Not Exactly” [mau5trap] – 10:20</li>
+      <li>Innate – “Imod (Jerome Isma-Ae remix)” [Baroque Limited Records] – 12:40</li>
+      <li>Deadmau5 – “Faxing Berlin (Chris Lake edit)” [mau5trap] – 16:40</li>
+      <li>Deadmau5 – “Jaded” [mau5trap] – 19:40</li>
+      <li>AxEr – “321” [Pryda Friends] – 23:00</li>
+      <li>Pryda – “Muranyi” [Pryda Friends] – 26:35</li>
+    </ol>
 
-**Notes**: 30 min progressive house mixtape, done with
-[Serato Scratch Live](https://serato.com/scratchlive) via
-[Technics SL 1200/1210](https://en.wikipedia.org/wiki/Technics_SL-1200) turntables and a
- [DJM 600 mixer](https://en.wikipedia.org/wiki/DJM). 6 months from my last mixtape, 1 year since I
- started out.
+    <p>
+     <strong>Notes</strong>: 30 min progressive house mixtape, done with{" "}
+     <a href="https://serato.com/scratchlive">Serato Scratch Live</a> via{" "}
+     <a href="https://en.wikipedia.org/wiki/Technics_SL-1200">Technics SL 1200/1210</a> turntables
+     and a <a href="https://en.wikipedia.org/wiki/DJM">DJM 600 mixer</a>. 6 months from my last
+     mixtape, 1 year since I started out.
+    </p>
+   </Layout>
+ );
+};
+
+export default Post;
