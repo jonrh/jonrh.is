ae0b854fed185eda52cedf126db735519c434653
Jón Rúnar Helgason <dev@jonrh.is>
2020-08-11 14:17:26 +0100

    Converted 8April 2007 mix from Markdown to JSX

--- src/pages/8april-2007-mix/index.js
+++ src/pages/8april-2007-mix/index.js
@@ -1,24 +1,34 @@
----
-title: 8April 2007 mix
-date: "2007-04-08T00:00:00Z"
-path: "/8april-2007-mix/"
----
+import React from "react";
+import Layout from "./../../layouts";
 
-Download link: 8April 2007 mix
+const Post = () => {
+  return (
+    <Layout>
+      <h1>8April 2007 mix</h1>
+      <p>April 08, 2007</p>
 
-## Tracklisting
+      <div>
+        <p>Download link: 8April 2007 mix</p>
+        <h2>Tracklisting</h2>
+        <ol>
+          <li>The Rhythm Purist – “Bassify (How Low Can You Go)” [Digital Jaxx UK] – 0:00</li>
+          <li>Dab Hands – “Supergood 07 Style (Vocal)” [Kidology Records ] – 5:00</li>
+          <li>UK Bastard vs. Jock McPhail – “Electrify (Miles Dyson remix)” [olavbelgoe.com] – 7:30</li>
+          <li>Robot Needs Oil – “Volta (Olivier Giacomot remis)” [Royal Flush Records] – 13:30</li>
+          <li>Enur ft. Natasha – “Calabria 2006 (Hoxton Whores remix)” [?] – 20:00</li>
+          <li>Flat Mode – “Play This Game” [Mylo Records] – 22:00</li>
+          <li>Melleefresh, Deadmau5 – “Something Inside Me” [Play Digital] – 27:30</li>
+          <li>Mind Electric – “Dirty Cash (Money Talks) (Dirty South remix)” [Vicious Grooves] – 31:30</li>
+          <li>Global Deejays – “Get Up ft. Technotronic (General Electric Version)” [Superstar Recordings] –
+            36:30</li>
+          <li>Dave Spoon – “This Machine ft. Penny Foster (Tom Neville remix)” [Toolrom] – 41:00</li>
+          <li>Mario Chris – “Color Dots” [Devilock Records] – 44:30</li>
+          <li>The Freaks – “The Creeps (Vandalism remix)” [Azuli Records] – 50:00</li>
+          <li>Roman Salzger – “Turn Up The Sun ft. Tiger Lily (Rough mix)” [Opaque Music] – 55:00</li>
+        </ol>
+      </div>
+    </Layout>
+  );
+};
 
-1. The Rhythm Purist – “Bassify (How Low Can You Go)” [Digital Jaxx UK] – 0:00
-2. Dab Hands – “Supergood 07 Style (Vocal)” [Kidology Records ] – 5:00
-3. UK Bastard vs. Jock McPhail – “Electrify (Miles Dyson remix)” [olavbelgoe.com] – 7:30
-4. Robot Needs Oil – “Volta (Olivier Giacomot remis)” [Royal Flush Records] – 13:30
-5. Enur ft. Natasha – “Calabria 2006 (Hoxton Whores remix)” [?] – 20:00
-6. Flat Mode – “Play This Game” [Mylo Records] – 22:00
-7. Melleefresh, Deadmau5 – “Something Inside Me” [Play Digital] – 27:30
-8. Mind Electric – “Dirty Cash (Money Talks) (Dirty South remix)” [Vicious Grooves] – 31:30
-9. Global Deejays – “Get Up ft. Technotronic (General Electric Version)” [Superstar Recordings] –
-36:30
-10. Dave Spoon – “This Machine ft. Penny Foster (Tom Neville remix)” [Toolrom] – 41:00
-11. Mario Chris – “Color Dots” [Devilock Records] – 44:30
-12. The Freaks – “The Creeps (Vandalism remix)” [Azuli Records] – 50:00
-13. Roman Salzger – “Turn Up The Sun ft. Tiger Lily (Rough mix)” [Opaque Music] – 55:00
+export default Post;
