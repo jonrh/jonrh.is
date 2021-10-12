75caede6c5fc6e37a30d66e744684eee352c89de
Jón Rúnar Helgason <dev@jonrh.is>
2021-10-12 16:58:24 -0700

    Remove emojis from front page
    Going for a more minimal look with fewer colours.

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ const Index = () => {
           for contractors." />
       </Head>
 
-      <h1>Hi, I'm Jón Rúnar 👋</h1>
+      <h1>Hi, I am Jón Rúnar</h1>
 
       <p>
         I am a cofounder of <a href="https://timavera.com">Tímavera</a>, a time tracking app that
         helps contractors eliminate the burden & guesswork of timesheets. From and currently
-        living in Iceland 🇮🇸. Lived for 6 years in Ireland 🇮🇪 between 2015 and 2021. If you want
+        living in Iceland. Lived for 6 years in Ireland between 2015 and 2021. If you want
         to get in touch please drop me an email at <a href="mailto:hi@jonrh.is">hi@jonrh.is</a>.
       </p>
     </Layout>
