231852f49c7e5df0f3f1907f05dbd41ae01182a3
Jón Rúnar Helgason <dev@jonrh.is>
2021-10-21 07:37:37 +0000

    Improve dithered images, added an option for .webp option, etc
    Added the `unoptimized={true}` flag to dithered images. By default the Next.js Image component uses quality={75}. The effect of that was that if I created for example an optimised dithered PNG version of an image that was say 25Kb Next.js Image would re-save that PNG as a WebP and the resulting file would look worse (lossy compression) and be about twice the size (50Kb).
    
    This prompted me to dig a little deeper. Turns out Safari has added support for WebP as of iOS 14. WebP achieves better compression than PNGs.
    
    In this commit I added a dark dithered version of the 1276x990 pixel screenshot in one blog post:
    
    Original: 251.037 bytes
    PNG: 22.885 bytes
    WebP: 14.840 bytes
    
    A monochrome dithered PNG is about 9% of the size of the original screenshot. The WebP is the exact same content (lossless) as the PNG but is 65% of the size of the PNG or 6% of the original. Total savings of about 8Kb for just one image. I would imagine the difference is even bigger for larger screenshots.
    
    So now the dithered images have a new option `webp`. If true it signals that I have made an optimized .webp version of the image that should be used instead. With time I aim to create a .webp version of all images and then I can stop using PNG files for dithering.

--- components/Dither.js
+++ components/Dither.js
@@ -4,15 +4,25 @@ import Image from "next/image";
 /**
  * A dithered image that links to the original source image in colour.
  *
- * The width and height props are for the dithered image.
+ * Props:
+ *   src: source of the original image
+ *   alt: image alt text
+ *   width: width of the dithered image
+ *   height: height of the dithered image
+ *   webp: true/false. If true it indicates I have created an optimised
+ *     WebP version which should be used instead of PNG. WebP typically
+ *     has a better compression ratio than PNG.
  */
-const Dither = ({src, alt, width, height}) => {
-  // Replace the .jpg / .jpeg / .gif / .png file ending with "-dither.png"
+const Dither = ({src, alt, width, height, webp}) => {
+  // Replace the .jpg / .jpeg / .gif / .png file
+  // ending with "-dither.png" or "-dither.webp".
   // File name convention:
-  //   "/images/post/image.jpg"         source image in colour
-  //   "/images/post/image-dither.png"  dithered version:
-  const ditherSrc = src.replace(/\.(jpe?g|gif|png)/, "-dither.png");
-  console.log(ditherSrc);
+  //   "/images/post/image.jpg"          source image in colour
+  //   "/images/post/image-dither.png"   dithered PNG version
+  //   "/images/post/image-dither.webp"  dithered Webp version if available
+  const ditherSrcPng = src.replace(/\.(jpe?g|gif|png)/, "-dither.png");
+  const ditherSrcWebp = src.replace(/\.(jpe?g|gif|png)/, "-dither.webp");
+  const ditherSrc = webp ? ditherSrcWebp : ditherSrcPng;
 
   return (
     <a href={src}>
@@ -21,6 +31,7 @@ const Dither = ({src, alt, width, height}) => {
         alt={alt}
         width={width}
         height={height}
+        unoptimized={true}
       />
     </a>
   );
