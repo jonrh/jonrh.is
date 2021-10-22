56bcf8bf15cdf712831c2a599e38e7dce7c136a5
Jón Rúnar Helgason <dev@jonrh.is>
2021-10-22 19:28:31 +0000

    Added a enforced 50-160 character meta description to all posts for SEO
    All posts must now have have a <meta> description that summarises in 50-160 characters the content of the post.
    
    If the meta description is missing, smaller than 50 characters or larger than 160 characters an error is thrown which halts the deployment of new versions.
    
    From what I could tell by a quick glance the range of 50-160 characters was recommended. It can technically of course be smaller or larger but from what I could tell you would increase the risk of search engines not using the meta description at all or that it would be truncated.
    
    Added a meta description to all existing posts. It was a fair bit of work so the descriptions themselves could be improved.

--- components/Post.js
+++ components/Post.js
@@ -9,13 +9,31 @@ const dateStyle = {
   marginBottom: "1.5em",
 };
 
+/**
+ * Ensure that each post has a meta description for better SEO. If the checks
+ * fail an error will be thrown and the build will fail. This ensures that a
+ * change can not be accidentally deployed.
+ */
+function metaDescriptionQualityCheck(metaDescription, title, date) {
+  if (!metaDescription) {
+    throw new Error(`Meta description is missing. Post: ${title}, date: ${date}.`);
+  }
+
+  if (metaDescription.length < 50 || metaDescription.length > 160) {
+    throw new Error(`Meta description is ${metaDescription.length} characters, should be 50-160.`);
+  }
+}
+
 /** A template for a blog post page */
-const Post = ({title, date, children}) => {
+const Post = ({title, date, metaDescription, children}) => {
+  metaDescriptionQualityCheck(metaDescription, title, date);
+
   return (
     <Layout>
       <Head>
         <title>{title}</title>
         <meta name="author" content="Jón Rúnar Helgason" />
+        <meta name="description" content={metaDescription} />
       </Head>
 
       <h1>{title}</h1>
