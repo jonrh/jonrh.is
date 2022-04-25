f58d4d671beaea513729db5fbd576861bb0a1e6e
Jón Rúnar Helgason <dev@jonrh.is>
2022-04-25 18:36:49 +0000

    Added new post: asdf with Nix on macOS

--- pages/blog.js
+++ pages/blog.js
@@ -35,6 +35,7 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <ul>
+        <li><a href="/asdf-with-nix-on-macos">asdf with Nix on macOS</a></li>
         <li><a href="/60x60-meditation-challenge">60x60 Meditation challenge</a></li>
         <li><a href="/2019-setup">My Remote Setup in 2019</a></li>
         <li><a href="/react-native-eu-2017-conference">React Native EU 2017 Conference</a></li>
